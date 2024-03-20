a17725fc99ed6acb2191a7da64eaa3e9b07fde57
João Víctor Santos Bueno <104871687+jvsbueno@users.noreply.github.com>
2024-03-20 11:33:31 -0300

    oi

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function atualizaCronometro {
 }
 
 atualizaCronometro();
-setInterval(atualizacronometro,1000);
+setInterval(atualizaCronometro,1000);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
