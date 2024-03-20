47b9422fc19ae08062d908d04172efdf70f07e68
João Víctor Santos Bueno <104871687+jvsbueno@users.noreply.github.com>
2024-03-20 11:58:39 -0300

    oi

--- main.js
+++ main.js
@@ -42,8 +42,11 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
+    if(tempoFinal>0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
+    }else{
+        return "Prazo finalizado";
+    }
 
 }
 
